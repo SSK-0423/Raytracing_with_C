5542fc50f4860e6a9cd9e982e69d7a8f9f1e0611
SSK-0423 <b1900446@planet.kanazawa-it.ac.jp>
2022-04-17 12:07:12 +0900

    libpngインストール

--- draw_test.cpp
+++ draw_test.cpp
@@ -2,36 +2,7 @@
 #include <malloc.h>
 #include <memory.h>
 
-// 色
-struct Color
-{
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-
-    Color(unsigned char red, unsigned char green, unsigned char blue)
-        : r(red), g(green), b(blue) {}
-};
-
-// ピクセル構造体
-struct Pixel
-{
-    Color color;
-    void SetColor(Color newColor) { color = newColor; }
-};
-
-// ビットマップデータ
-struct BitMapData
-{
-    unsigned int width;    // 幅
-    unsigned int height;   // 高さ
-    Pixel *pixelsData;     // ピクセルデータ
-    unsigned char channel; // チャネル
-
-    BitMapData(unsigned int w, unsigned int h, unsigned int ch)
-        : width(w), height(h), channel(ch) {}
-};
-
+#include "myPng.h"
 
 // 点の描画
 void drawDot(
